40e74b517ef8cb9438e983962330f3eded81e55f
jpantonow <jpantonow@gmail.com>
2023-06-13 20:54:24 -0300

    ok

---
+++ camadas.cpp
@@ -0,0 +1,22 @@
+#include "camadafisica.hpp"
+void CamadaAplicacao::Transmissora(vector <int> quadro){
+cout << "Escolha uma mensagem a ser codificada: " << endl;
+cin >> mensagem;
+
+}
+void CamadaAplicacao::Receptora(vector <int> quadro){
+
+}
+void CamadaAplicacao::Mensagem(string mensagem){
+
+}
+
+int CamadaFisica::TransmissoraBinaria(){
+
+}
+int CamadaFisica::TransmissoraManchester(){
+
+}
+int CamadaFisica::TransmissoraBipolar(){
+
+}
