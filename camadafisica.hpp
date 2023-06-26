46c433714cad34a4a7f1f4ddca0f4c31452f6ffb
jpantonow <jpantonow@gmail.com>
2023-06-26 08:25:48 -0300

    documentação

--- camadafisica.hpp
+++ camadafisica.hpp
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <vector>
 using namespace std;
+/**
+ * @brief Superclasse que representa uma camada genérica.
+ * 
+ * Tem como atributos públicos e herdáveis no modo public o quadro, o fluxo bruto de bits e um inteiro que representa o tipo de codificação.
+ * 
+ */
 class Camada {
     public:
     vector <int> quadro;
@@ -11,17 +17,77 @@ class Camada {
     int codificacao;
 };
 
+/**
+ * @brief Classe que representa a camada física transmissora.
+ * 
+ * Herda no modo public a classe Camada.
+ * 
+ * Representa a camada que irá codificar a mensagem e transmití-la adiante.
+ * 
+ */
 class CamadaFisicaTransmissora: public Camada {
     private:
+    /**
+     * @brief Constante para representar a codificação binaria.
+     * 
+     */
     const static int BINARIA = 0;
+    /**
+     * @brief Constante para representar a codificação manchester.
+     * 
+     */
     const static int MANCHESTER = 1;
+    /**
+     * @brief Constante para representar a codificação bipolar.
+     * 
+     */
     const static int BIPOLAR = 2;
+    /**
+     * @brief Vetor de inteiros que representa o clock.
+     * 
+     * Possui os inteiros 0 e 1, a fim de representar os pulsos do clock.
+     * 
+     */
     vector <int> CLOCK = {0, 1};
     public:
+    /**
+     * @brief Método que inicia a codificação.
+     * 
+     * Recebe a mensagem e o tipo de codificação a ser escolhida, para depois transmití-la.
+     * 
+     * Tem em suas estruturas os métodos para codificar a mensagem, o canal para transmití-la e a camada de aplicacao receptora ao final do processo. 
+     * 
+     * @param codificacao 
+     * @param mensagem 
+     */
     void iniciar(int codificacao, string mensagem);
+    /**
+     * @brief Método que codifica e transmite a mensagem na codificação binaria.
+     * 
+     * @param quadro 
+     * @return vector <int> 
+     */
     vector <int> TransmissoraBinaria(vector <int> quadro);
+    /**
+     * @brief Método que codifica e transmite a mensagem na codificação manchester.
+     * 
+     * @param quadro 
+     * @return vector <int> 
+     */
     vector <int> TransmissoraManchester(vector <int> quadro);
+    /**
+     * @brief Método que codifica e transmite a mensagem na codificação bipolar.
+     * 
+     * @param quadro 
+     * @return vector <int> 
+     */
     vector <int> TransmissoraBipolar(vector <int> quadro);
+    /**
+     * @brief Método que recebe a string mensagem e a transforma em bits.
+     * 
+     * @param mensagem 
+     * @return vector <int> 
+     */
     vector <int> ConversorStringBits(string mensagem);
 };
 class CamadaFisicaReceptora: public Camada {
