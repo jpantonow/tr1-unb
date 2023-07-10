2b02fa7682a467dc35f49dd535906af579069db2
jpantonow <jpantonow@gmail.com>
2023-07-10 08:25:39 -0300

    atualização cpp

---
+++ camadaenlace.cpp
@@ -0,0 +1,29 @@
+#include "camadaenlace.hpp"
+
+int CamadaEnlaceTransmissora::ContagemDeCaracteres(vector <int> quadro){
+
+}
+
+vector <int> CamadaEnlaceTransmissora::InsercaoDeBytes(vector <int> quadro){
+
+}
+vector <int> CamadaEnlaceTransmissora::ControleDeErroCRC(vector <int> quadro){
+
+}
+
+vector <int> CamadaEnlaceTransmissora::ControleDeErroBitParidadePar(vector <int> quadro){
+
+}
+
+int CamadaEnlaceReceptora::ContagemDeCaracteres(vector <int> quadro){
+
+}
+vector <int> CamadaEnlaceReceptora::InsercaoDeBytes(vector <int> quadro){
+
+}
+vector <int> CamadaEnlaceReceptora::InsercaoDeBytes(vector <int> quadro){
+
+}
+vector <int> CamadaEnlaceReceptora::InsercaoDeBytes(vector <int> quadro){
+
+}
